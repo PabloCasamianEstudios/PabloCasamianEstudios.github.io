470d52dc17cf5ded2628a8184962bb4e05f2e7b8
PabloCasamianEstudios <pablocasamianestudios@gmail.com>
2026-05-11 19:06:40 +0200

    feat: Project Initialization

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="hello-world">
+        <h1>Hello World</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
